1c70d07462f56cecf4e235504201202a764eca22
ElnurKasimov <ekasimov2012@gmail.com>
2024-04-21 21:07:06 +0300

    sortByBubble(int[] array) has been covered by test

--- src/test/java/sort/SortingTest.java
+++ src/test/java/sort/SortingTest.java
@@ -8,17 +8,24 @@ import java.util.stream.Stream;
 import static org.junit.jupiter.api.Assertions.*;
 
 class SortingTest {
-    @ParameterizedTest(name = "{index} test that in array {0} has been sorted correctly")
-    @MethodSource
+    @ParameterizedTest(name = "{index} test that in array {0} has been sorted by selection correctly")
+    @MethodSource("provideArrays")
     void testForSortingBySelectionForDifferentCases(int[] inputArray, int[] sortedArray) {
         assertArrayEquals(sortedArray, Sorting.sortBySelection(inputArray));
     }
 
-    private static Stream<Arguments> testForSortingBySelectionForDifferentCases() {
+    @ParameterizedTest(name = "{index} test that in array {0} has been sorted by bubble correctly")
+    @MethodSource("provideArrays")
+    void testForSortingByBubbleForDifferentCases(int[] inputArray, int[] sortedArray) {
+        assertArrayEquals(sortedArray, Sorting.sortByBubble(inputArray));
+    }
+
+    private static Stream<Arguments> provideArrays() {
         return Stream.of(
                 Arguments.of(new int[]{6,5,4,3,2,1}, new int[]{1,2,3,4,5,6}),
                 Arguments.of(new int[]{0,0,0,0,0}, new int[]{0,0,0,0,0}),
                 Arguments.of(new int[]{-3,-68,23,0,56},new int[]{-68,-3,0,23,56}),
+                Arguments.of(new int[]{-3,-68,23,-68,0,56, 23},new int[]{-68,-68,-3,0,23,23,56}),
                 Arguments.of(new int[]{}, new int[]{})
         );
     }
