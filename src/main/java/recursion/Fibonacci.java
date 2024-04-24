9bd2da7f59fbd30318c77ae0e8fdca8681b143c0
Elnur Kasimov <ekasimov2012@gmail.com>
2024-04-24 11:02:14 +0300

    1. added Fibonacci.java into recursion; 2. covered its method by tests

--- src/main/java/recursion/Fibonacci.java
+++ src/main/java/recursion/Fibonacci.java
@@ -1,12 +1,15 @@
 package recursion;
 
-public class Fibonacсi {
+public class Fibonacci {
 
     public int calculateFibonacci(int number) {
-        if (number <= 0 ) return 0;
-        else if(number == 1) return 1;
-        else {
+        if (number <= 0 ) {
+            return 0;
+        } else if(number == 1) {
+            return 1;
+        } else {
             return calculateFibonacci(number - 1) + calculateFibonacci(number - 2);
         }
     }
+
 }
--- src/test/java/recursion/FibonacciTest.java
+++ src/test/java/recursion/FibonacciTest.java
@@ -1,6 +1,5 @@
 package recursion;
 
-import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
@@ -9,19 +8,19 @@ import java.util.stream.Stream;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-class FactorialTest {
+class FibonacciTest {
 
-    private Factorial factorial = new Factorial();
+    private final Fibonacci fibonacci = new Fibonacci();
     @ParameterizedTest(name = "{index}. test that factorial({0}) is equal {1}")
-    @MethodSource("provideDataForFactorialCalculation")
+    @MethodSource("provideDataForFibonacciCalculation")
     void testThatFactorialCalculationIsCorrect (int number, long expected) {
-        assertEquals(expected, factorial.calculateFactorial(number));
+        assertEquals(expected, fibonacci.calculateFibonacci(number));
     }
 
-    private static Stream<Arguments> provideDataForFactorialCalculation () {
+    private static Stream<Arguments> provideDataForFibonacciCalculation () {
         return Stream.of(
                 Arguments.of(1, 1),
-                Arguments.of(5, 120),
+                Arguments.of(10, 55),
                 Arguments.of(0,0),
                 Arguments.of(-2, 0)
         );
